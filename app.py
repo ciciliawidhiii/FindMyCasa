b0a930b1f5aebaac4feff24c98ab6dbed57235bb
ciciliawidhiii <ciciliawidhi1@gmail.com>
2023-06-03 11:44:09 +0700

    build backend using flask

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, Response
+import numpy as np 
+import sklearn
 
 app = Flask(__name__)
 
-@app.roure("/Main")
-def
+import pickle 
+regressor = pickle.load(open('model_lr.pkl', 'rb'))
+
+#@app.route("/Main")
+#def hello_world(request):
+        #result = regressor.predict([1000, 100, 5, 4, 1])
+
+        #return Response(int(result))
+
+@app.route('/prediction')
+def index():
+    LT = request.args.get('LT')
+    LB = request.args.get('LB')
+    JKT = request.args.get('JKT')
+    JKM = request.args.get('JKM')
+    GRS = request.args.get('GRS')
+
+    LT = float(LT)
+    LB = float(LB)
+    JKT = float(JKT)
+    JKM = float(JKM)
+    GRS = float(GRS)
+
+    result = regressor.predict([[LT, LB, JKT, JKM, GRS]])
+
+
+    res = {
+        'result': result[0]
+    }
+    return res
+    #return Response(result, mimetype='application/json')
+
